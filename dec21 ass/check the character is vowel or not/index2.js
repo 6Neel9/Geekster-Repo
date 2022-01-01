d9999bda2d8b5d82f3e2eb8d0e6aae195d7e2471
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2022-01-01 10:23:40 +0530

    Add files via upload

---
+++ dec21 ass/check the character is vowel or not/index2.js
@@ -0,0 +1,16 @@
+var char = prompt("Enter Character");
+
+if((char == "a") || (char == "A")) {
+ alert("It is a vowel");
+} else if((char == "e" ) || (char == "E")) {
+ alert("It is a vowel");
+} else if((char == "i" ) || (char == "I")) {
+ alert("It is a vowel");
+} else if((char == "o" ) || (char == "O")) {
+ alert("It is a vowel");
+} else if((char == "u" ) || (char == "U")) {
+ alert("It is a vowel");
+} else {
+ alert("It is a consonant");
+}
+
---
+++ dec21 ass/three numbers find minimum/index.js
@@ -0,0 +1,14 @@
+
+
+    var a = prompt("Enter num1: ");
+    var b = prompt("Enter num2: ");
+    var c = prompt("Enter num3: ");
+    
+    if (a < b && a < c)
+        alert( a + " is the smallest");
+ 
+    else if (b < a && b < c)
+        alert( b + " is the smallest");
+ 
+    else
+        alert( c + " is the smallest");
