14f33cfeb88e17bd97794ce21e990cb4fc84cc99
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2022-01-01 10:06:47 +0530

    Add files via upload

---
+++ dec 20 ass/check greater or smaller than 10/ass1.js
@@ -0,0 +1,6 @@
+var num=5;
+if(num>10) {
+	console.log("it's greater than 10")
+}else {
+	console.log("it's smaller than 10")
+}
---
+++ dec 20 ass/find the greatest among 3 nums/ass2.js
@@ -0,0 +1,16 @@
+var num1 = 50;
+var num2 = 90;
+var num3 = 70;
+
+
+if(num1 >num2 && num1 > num3) {
+    console.log(" Biggest number is " + num1)
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log("Biggest num is "+  num2)
+}
+else {
+    console.log("Biggest num is " + num3)
+}
+
+
