d227bc45bd2375b76fe515690ae60f12f76bebe7
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2022-01-01 13:01:34 +0530

    Add files via upload

---
+++ dec 22 ass/array add element/index.js
@@ -0,0 +1,7 @@
+var a = [];
+a.push(parseInt(prompt("Enter num 1: ")));
+a.push(parseInt(prompt("Enter num 2: ")));
+a.push(parseInt(prompt("Enter num 3: ")));
+console.log(a);
+console.log(a.length);
+alert(Math.max(...a));
