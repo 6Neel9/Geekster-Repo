df3915921d185d70a10702ddfdc739658598c0ac
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2022-01-01 11:35:17 +0530

    Update index.js

--- dec21 ass/three numbers find minimum/index.js
+++ dec21 ass/three numbers find minimum/index.js
@@ -1,7 +1,7 @@
 
-    var a = prompt("Enter num1: ");
-    var b = prompt("Enter num2: ");
-    var c = prompt("Enter num3: ");
+    var a = parseInt(prompt("Enter num1: "));
+    var b = parseInt(prompt("Enter num2: "));
+    var c = parseInt(prompt("Enter num3: "));
     
     if (a <= b && a <= c)
         alert( a + " is the smallest");
