c643b7ba6427228ad13b312dd560913bedc6b421
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2022-01-19 10:23:51 +0530

    Add files via upload

---
+++ 17 jan ass/script.js
@@ -0,0 +1,16 @@
+var num = parseInt(prompt("Enter your number"))
+function manArr(n,callback1,callback2){
+    var mul = callback1(n);
+    var add = callback2(n);
+    return [mul , add] ;
+}
+function mulBy4(x){
+    return x * 4;
+}
+function add9(x){
+    return x + 9;
+}
+var func = manArr(num,mulBy4,add9);
+console.log(func);
+document.write(func);
+
